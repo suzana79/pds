9a696b1444c41327b9c97e67cbcbcef65bccd8f9
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-02-23 19:42:03 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,17 @@
+mes = input("Digite um mês:")
+if mes in ("Dezembro","Janeiro","Fevereiro"): 
+    estacao = "Verão" 
+
+elif mes in ("Marco","Abril","Maio"): 
+    estacao = "Outono"
+    
+elif mes in ("Junho","Julho","Agosto"): 
+    estacao = "Inverno"
+    
+elif mes in ("Junho","Julho","Agosto"): 
+    estacao = "Inverno"
+        
+elif mes in ("setembro","outubro","novembro"):
+    estacao = "primavera"
+    
+print(f"A estação do seu mês é: {estacao}.")
