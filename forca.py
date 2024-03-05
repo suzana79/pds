71799647034337e289742b191b40ac9d0123ab38
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-03-05 08:26:33 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,30 @@
+    def jogar():
+        print("*************")
+        print("****")
+        print("*************")
+    palavra_secreta = "paraiso"
+    letras_acertadas = ["_","_","_","_","_","_","_"]
+    tentativas = 3 
+
+    while tentativas > 0 and "_" in letras_acertadas:
+        palpite = input("Digite uma letra:").lower()
+        
+        if palpite in palavra_secreta: 
+            index = 0 
+            for letra in palavra_secreta :
+                if palpite == letra :
+                    letras_acertadas [index] = letra
+                index += 1 
+        else : 
+            tentativas -= 1
+            print (f"Você tem {tentativas} tentativas.")
+        
+        print (" ".join(letras_acertadas))
+            
+    if "_" not in letras_acertadas : 
+        print ("PARABÉNS VC GANHOU!!!")
+    else :
+        print (f"perdeu AAKAKAKAKAKKA. A palavra era:", palavra_secreta)
+        
+if(__name__ == "__main__"):
+    jogar()
