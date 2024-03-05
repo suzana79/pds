1102a347e93024b4173be834f4661b5ab7f6769a
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-03-05 08:26:48 -0300

    Add files via upload

---
+++ jogos.py
@@ -0,0 +1,17 @@
+    import forca
+    import adivinhacao
+    def escolha_jogo():
+        print("*************")
+        print("**Escolha o jogo**")
+        print("*************")
+        print("(1)Forca,(2)Adivinhação")
+        jogo = int(input("Escolha o jogo"))
+        if(jogo == 1):
+            print("Jogando forca")
+            forca.jogar()
+        elif(jogo == 2):
+            print("Jogando adivinhacao")
+            adivinhacao.jogar_adivinhacao()
+
+if(__name__ == "__main__"):
+    escolha_jogo()
