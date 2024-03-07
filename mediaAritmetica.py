039e5c5e3a33d4b4fc9a796c8a65945a20c35b5b
suzana79 <128698630+suzana79@users.noreply.github.com>
2024-03-07 12:11:11 -0300

    Add files via upload

---
+++ mediaAritmetica.py
@@ -0,0 +1,6 @@
+nota1 = int(input("Digite um número"))
+nota2 = int(input("Digite um número"))
+nota3 = int(input("Digite um número"))
+resultado= (nota1 + nota2 + nota3/3)
+
+print(f"O resultado é {resultado}")
